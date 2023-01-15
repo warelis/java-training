17ecffc7ebbd55fe230a7a3e05da98eee1f95aa2
warelis <122726481+warelis@users.noreply.github.com>
2023-01-15 12:12:26 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,65 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /* 1 Zadanie
+        * 500g mąki pszennej
+        * 14 g drożdży
+        * 50g cukru
+        * 1 łyżeczka soli
+        * 300 ml mleka
+        * 50 g masła
+        * xd
+        */
+        int maka = 500;
+        int drozdze = 14;
+        int cukier = 50;
+        int lyzeczka_soli = 1;
+        int mleko = 300;
+        int maslo = 50;
+
+        /* 2 Zadanie - nie */
+
+        float xd = 3.14f;
+
+        /* Zadanie 3
+         */
+        System.out.println("\\n");
+
+        /* Zadanie 4 Liczba się pomniejszy wg 0 1 zasady którą tłumaczyłeś i zamieni się na -
+         */
+        long big = 3_000_000_0000L;
+        System.out.println(big);
+        int small = (int)big;
+        System.out.println(small);
+
+        /* Zadanie 5
+         */
+        float picie = 1.1f + 1.5f;
+        System.out.printf("%.10f\n", picie);
+
+        /* Zadanie 6
+         */
+        int wiekKlient;
+        int wiekZwierze;
+        String imiePupil;
+        boolean pelnoletnosc;
+        char rozmiarUbrania;
+        String cytat;
+
+        /* Zadanie 7
+         */
+        char a = 'a';
+        int aKonwert = (int)a;
+        System.out.println(aKonwert);
+        char duzeA = 'A';
+        int duzeAKonwert = (int)duzeA;
+        System.out.println(duzeAKonwert);
+        char z = 'z';
+        int zKonwert = (int)z;
+        System.out.println(zKonwert);
+        char duzeZ = 'Z';
+        int duzeZKonwert = (int)duzeZ;
+        System.out.println(duzeZKonwert);
+
+    }
+}
