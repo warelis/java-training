1c97ad9b1d645b288060b20c9d5c212332d324b0
Patrycja Wareliś <122726481+warelis@users.noreply.github.com>
2023-10-01 14:22:19 +0200

    Add files via upload

---
+++ Tablice/Main.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+    //Ex1
+//    public static int sumOf(int[] values){
+//        int sum = 0;
+//        for(int x : values){
+//            sum += x;
+//        }
+//        return sum;
+//    }
+
+    //Ex2
+//    public static int sumOf(int[] values, int greaterThen){
+//        int sum = 0;
+//        for(int x : values) {
+//            if (x > greaterThen) {
+//                sum += x;
+//            }
+//        }
+//        return sum;
+//    }
+
+    //Ex3
+//      public static int sumOf(int howMany){
+//          Scanner scan = new Scanner(System.in);
+//          int[] values = new int[howMany];
+//          int sum = 0;
+//          for(int i = 0; i < howMany; i++){
+//              System.out.println("Give" + " " + (i + 1) + " " + "number: ");
+//              int value = scan.nextInt();
+//              System.out.println(value);
+//              values[i] = value;
+//              sum += value;
+//          }
+//          return sum;
+//      }
+
+    //Ex4
+
+
+    public static void main(String[] args) {
+//       Ex1
+//        int[] values = {3, 1};
+//        System.out.println(sumOf(values));
+
+//       Ex2
+//        int[] values = {1, 3, 6, 14};
+//        int result = sumOf(values, 3);
+//        System.out.println(result);
+
+//       Ex3
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("How many numbers are you going to give?");
+//        int howMany = scan.nextInt();
+//        int result = sumOf(howMany);
+//        System.out.println(result);
+
+        //Ex4
+
+
+
+
+
+        }
+    }
